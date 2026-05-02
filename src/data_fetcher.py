a29b6e3e1e77d4316873f04dbd3dcc34bd80c68d
dlxodh <122535954+dlxodh@users.noreply.github.com>
2026-05-02 16:50:14 -0400

    Add files via upload

--- src/data_fetcher.py
+++ src/data_fetcher.py
@@ -6,7 +6,7 @@ def get_latest_version():
     return requests.get(versions_url).json()[0]
 
 # loads item data / Korean mode
-def fetch_items(version = None, language = "en_US"):
+def fetch_items(version = None, language = "ko_KR"):
     if version is None:
         version = get_latest_version()
     url = f"https://ddragon.leagueoflegends.com/cdn/{version}/data/{language}/item.json"
